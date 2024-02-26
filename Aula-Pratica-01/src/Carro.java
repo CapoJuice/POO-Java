ed19210990e2657bba93d03fb0d2f9c037da1344
CapoJuice <atilacrrofc@gmail.com>
2024-02-26 19:46:48 -0300

    anotacao

--- Aula-Pratica-01/src/Carro.java
+++ Aula-Pratica-01/src/Carro.java
@@ -1,11 +1,12 @@
+
+//classe de um objeto sem construtores
+
 public class Carro {
     
     String nome;
     String marca;
     int ano;
     int velo;
-
-
 
     void acelerar(int aceleracao) {
         velo += aceleracao;
